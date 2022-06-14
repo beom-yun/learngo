a5f822cb99900f3e28f5004b66fb65e27a4373d1
beom-yun <scds0326@naver.com>
2022-06-14 15:19:38 +0900

    Functions part Two

--- main.go
+++ main.go
@@ -5,17 +5,13 @@ import (
 	"strings"
 )
 
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
-}
-
-func repeatMe(words ...string) {
-	fmt.Println(words)
+func lenAndUpper(name string) (length int, uppercase string) {
+	defer fmt.Println("I'm done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func main() {
-	totalLength, upperName := lenAndUpper("y u n")
-	fmt.Println(totalLength, upperName)
-
-	repeatMe("kim", "yun", "song", "park", "jeong")
+	fmt.Println(lenAndUpper("yun"))
 }

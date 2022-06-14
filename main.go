2c1fd67754483862074739050411d61a300097c2
beom-yun <scds0326@naver.com>
2022-06-14 15:29:24 +0900

    for, range, ...args

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func lenAndUpper(name string) (length int, uppercase string) {
-	defer fmt.Println("I'm done")
-	length = len(name)
-	uppercase = strings.ToUpper(name)
-	return
+func superAdd(numbers ...int) int {
+	result := 0
+	for _, n := range numbers {
+		result += n
+	}
+	return result
 }
 
 func main() {
-	fmt.Println(lenAndUpper("yun"))
+	total := superAdd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+	fmt.Println(total)
 }

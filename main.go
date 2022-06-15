d60e94084ca7a3f613ecabd32420ad75bc9e64ce
beom-yun <scds0326@naver.com>
2022-06-15 11:22:27 +0900

    Maps

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	names := []string{"yun", "kim", "park"}
-	names = append(names, "jeong")
-	fmt.Println(names)
+	yun := map[string]string{"name": "yun", "age": "31"}
+	for key, value := range yun {
+		fmt.Println(key, value)
+	}
 }

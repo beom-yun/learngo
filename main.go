0722ec3eca6370285071e81e37d2d5420e8b32d5
beom-yun <scds0326@naver.com>
2022-06-15 11:09:33 +0900

    Pointers

--- main.go
+++ main.go
@@ -2,22 +2,9 @@ package main
 
 import "fmt"
 
-func canIDrink(age int) bool {
-	switch koreanAge := age + 2; koreanAge {
-	case 10:
-		return false
-	case 50:
-		return false
-	}
-	return true
-
-	// switch {
-	// case age < 10:
-	// 	return false
-	// }
-	// return true
-}
-
 func main() {
-	fmt.Println(canIDrink(18))
+	a := 2
+	b := &a
+	*b = 20
+	fmt.Println(a, *b)
 }
